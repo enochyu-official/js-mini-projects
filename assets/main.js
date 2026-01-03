ae01f6d4aee3070fb53cab28b7549684e021f570
Enoch Yu <247741827+enochyu-official@users.noreply.github.com>
2026-01-03 11:28:44 +0900

    gc-implement

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,17 @@
+fetch('/layouts/partials/footer.html')
+  .then(response => response.text())
+  .then(htmlString => {
+    const target = document.getElementById('footer-import');
+    target.innerHTML = htmlString;
+    
+    target.querySelectorAll('script').forEach(oldScript => {
+      const newScript = document.createElement('script');
 
+      newScript.textContent = oldScript.textContent;
+      Array.from(oldScript.attributes).forEach(attr => 
+        newScript.setAttribute(attr.name, attr.value)
+      );
+      oldScript.replaceWith(newScript);
+    });
+  })
+  .catch(error => console.error('Error loading footer:', error));
